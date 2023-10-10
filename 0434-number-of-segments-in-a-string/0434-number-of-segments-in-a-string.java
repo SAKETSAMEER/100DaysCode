e1fc8bd18737b9e4686780158871500dacb5ad32
Saket Sameer <91389452+SAKETSAMEER@users.noreply.github.com>
2023-10-11 00:17:48 +0530

    Time: 0 ms (100.00%), Space: 40.1 MB (68.19%) - LeetHub

---
+++ 0434-number-of-segments-in-a-string/0434-number-of-segments-in-a-string.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Solution {
+
+    public int countSegments(String s) {
+        StringTokenizer st=new StringTokenizer(s," ");
+        int count=0;
+        while(st.hasMoreTokens())
+        {
+            count++;
+            st.nextToken();
+        }
+
+        return count;
+    }
+}
