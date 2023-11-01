a320f0773aa716fab4c548f0ed81a7512428b0ab
Saket Sameer <91389452+SAKETSAMEER@users.noreply.github.com>
2023-11-01 23:44:44 +0530

    Time: 6 ms (97.89%), Space: 43.2 MB (57.53%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,25 +1,49 @@
-
-class Solution {
-  public String longestPalindrome(String s) {
-    int start = 0;
-    int end = 0;
-    for (int i = 0; i < s.length(); i++) {
-      int lenOne = helper(s, i, i);
-      int lenTwo = helper(s, i, i + 1);
-      int maxLength = Math.max(lenOne, lenTwo);
-      if (maxLength > end - start) {
-        start = i - (maxLength - 1) / 2;
-        end = i + maxLength / 2;
-      }
+public class Solution {
+    public String longestPalindrome(String s) {
+        // Insert boundary characters to handle even-length palindromes
+        StringBuilder modifiedString = new StringBuilder("^#");
+        for (char c : s.toCharArray()) {
+            modifiedString.append(c).append("#");
+        }
+        modifiedString.append("$");
+        String modifiedStr = modifiedString.toString();
+        
+        int len = modifiedStr.length();
+        int[] palindromeLengths = new int[len];
+        int center = 0, rightBoundary = 0;
+        
+        for (int i = 1; i < len - 1; i++) {
+            if (i < rightBoundary) {
+                int mirror = 2 * center - i;
+                palindromeLengths[i] = Math.min(rightBoundary - i, palindromeLengths[mirror]);
+            }
+            
+            // Attempt to expand palindrome centered at i
+            while (modifiedStr.charAt(i + 1 + palindromeLengths[i]) == modifiedStr.charAt(i - 1 - palindromeLengths[i])) {
+                palindromeLengths[i]++;
+            }
+            
+            // If this palindrome's right boundary exceeds the rightmost boundary so far,
+            // adjust center and right boundary
+            if (i + palindromeLengths[i] > rightBoundary) {
+                center = i;
+                rightBoundary = i + palindromeLengths[i];
+            }
+        }
+        
+        // Find the maximum length of palindromic substring
+        int maxLen = 0;
+        int centerIndex = 0;
+        for (int i = 0; i < len; i++) {
+            if (palindromeLengths[i] > maxLen) {
+                maxLen = palindromeLengths[i];
+                centerIndex = i;
+            }
+        }
+        
+        // Calculate original string indices from modified string
+        int start = (centerIndex - maxLen) / 2;
+        int end = start + maxLen;
+        return s.substring(start, end);
     }
-    return s.substring(start, end + 1);
-  }
-  
-  private int helper(String s, int left, int right) {
-    while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
-      left--;
-      right++;
-    }
-    return right - left - 1;
-  }
 }
